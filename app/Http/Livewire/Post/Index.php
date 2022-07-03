fbe3f8a1d78bd47d50f91c50cc396d2ac566b0aa
supardi_9512 <supardi@atomic.id>
2022-07-03 22:17:48 +0700

    read (get) data

--- app/Http/Livewire/Post/Index.php
+++ app/Http/Livewire/Post/Index.php
@@ -3,14 +3,14 @@
 namespace App\Http\Livewire\Post;
 
 use Livewire\Component;
+use App\Models\Post;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.post.index');
-
-        // Custom layouts
-        // return view('livewire.post.index')->layout('app');
+        return view('livewire.post.index', [
+            'posts' => Post::latest()->paginate(5)
+        ])->extends('layouts.app')->section('content');
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,14 +7,23 @@
     <title>CRUD SPA Laravel 9 & Livewire 2</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">    
     @livewireStyles
-    @livewireScripts
 </head>
 <body>
     <div class="container mt-5 mb-5">
         <div class="row justify-content-center">
             <div class="col-md-12">
+
+                @if (session()->has('message'))
+                    <div class="alert alert-success">
+                        {{ session('message') }}
+                    </div>
+                @endif
+
+                @yield('content')
+
             </div>
         </div>
     </div>
+    @livewireScripts
 </body>
 </html>
